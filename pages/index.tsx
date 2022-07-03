c57bf48c0976aaf36d7cc85d7a385314cc68a68e
nedakinduryte <neda.kinduryte@gmail.com>
2022-07-03 16:55:04 +0100

    added Box component to the homepage & added new styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,19 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import Image from 'next/image';
+
+import Box from 'components/Box';
+
+import styles from 'styles/pages/homepage.module.css';
 
 const Home: NextPage = () => {
-  return (
-    <div>
-  
-    </div>
-  )
-}
+	return (
+		<>
+			<section className={styles.hero}>
+				<Box />
+			</section>
+		</>
+	);
+};
 
-export default Home
+export default Home;
