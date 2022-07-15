11168070017f2c0455886e8580c4eea69fb00264
nedakinduryte <neda.kinduryte@gmail.com>
2022-07-15 18:53:45 +0100

    added subheading & icons to the homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,12 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import Image from 'next/image';
+import Link from 'next/link';
 
 import Box from 'components/Box';
+import LinkedInIcon from 'public/icons/linkedin.svg';
+import GithubIcon from 'public/icons/github.svg';
+import EmailIcon from 'public/icons/email.svg';
+import CvIcon from 'public/icons/cv.svg';
 
 import styles from 'styles/pages/homepage.module.css';
 
@@ -11,6 +15,27 @@ const Home: NextPage = () => {
 		<>
 			<section className={styles.hero}>
 				<Box />
+				<h2 className={styles.subheading}>Front-end engineer</h2>
+				<div className={styles.links}>
+					<Link href="https://www.linkedin.com/in/neda-danilevice-19ba4114a">
+						<a>
+							<LinkedInIcon className={styles.icon} />
+						</a>
+					</Link>
+					<Link href="https://github.com/nedakinduryte">
+						<a>
+							<GithubIcon className={styles.icon} />
+						</a>
+					</Link>
+					<Link href="mailto:neda.danilevice@gmail.com">
+						<a>
+							<EmailIcon className={styles.icon} />
+						</a>
+					</Link>
+					<button>
+						<CvIcon className={styles.icon} />
+					</button>
+				</div>
 			</section>
 		</>
 	);
