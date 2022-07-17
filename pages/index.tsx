457b7558ff14a506bb51f59ee6442279c5f9e2d8
nedakinduryte <neda.kinduryte@gmail.com>
2022-07-17 15:08:37 +0100

    added cv

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,34 +11,36 @@ import CvIcon from 'public/icons/cv.svg';
 import styles from 'styles/pages/homepage.module.css';
 
 const Home: NextPage = () => {
-	return (
-		<>
-			<section className={styles.hero}>
-				<Box />
-				<h2 className={styles.subheading}>Front-end engineer</h2>
-				<div className={styles.links}>
-					<Link href="https://www.linkedin.com/in/neda-danilevice-19ba4114a">
-						<a>
-							<LinkedInIcon className={styles.icon} />
-						</a>
-					</Link>
-					<Link href="https://github.com/nedakinduryte">
-						<a>
-							<GithubIcon className={styles.icon} />
-						</a>
-					</Link>
-					<Link href="mailto:neda.danilevice@gmail.com">
-						<a>
-							<EmailIcon className={styles.icon} />
-						</a>
-					</Link>
-					<button>
-						<CvIcon className={styles.icon} />
-					</button>
-				</div>
-			</section>
-		</>
-	);
+  return (
+    <>
+      <section className={styles.hero}>
+        <Box />
+        <h2 className={styles.subheading}>Front-end engineer</h2>
+        <div className={styles.links}>
+          <Link href="https://www.linkedin.com/in/neda-danilevice-19ba4114a">
+            <a>
+              <LinkedInIcon className={styles.icon} />
+            </a>
+          </Link>
+          <Link href="https://github.com/nedakinduryte">
+            <a>
+              <GithubIcon className={styles.icon} />
+            </a>
+          </Link>
+          <Link href="mailto:neda.danilevice@gmail.com">
+            <a>
+              <EmailIcon className={styles.icon} />
+            </a>
+          </Link>
+          <Link href="/Neda-Danilevice-cv.pdf">
+            <a target="_blank" rel="noopener noreferrer">
+              <CvIcon className={styles.icon} />
+            </a>
+          </Link>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Home;
